1004bafddf3beaf64a5eddfbc82d511d2f257ac3
York Xiang <bombless@126.com>
2025-11-28 17:35:23 +0800

    将msys2脚本改为cmd.exe使用的脚本

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-
-mkdir -p EFI\BOOT
-cp hello.efi EFI\BOOT\BOOTX64.EFI
+nasm -f bin hello_efi.asm -o hello.efi
+mkdir EFI\BOOT
+copy hello.efi EFI\BOOT\BOOTX64.EFI
 qemu-system-x86_64 -bios DEBUGX64_OVMF.fd -drive format=raw,file=fat:rw:.
